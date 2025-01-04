139c3ffa18f67c2dcdf1da2d315107466f9519c8
Gabrieldsmon <gabrieldsmon@gmail.com>
2025-01-04 09:48:00 -0300

    Projeto de um Boletim Estudantil mostrado a Identações

---
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,25 @@
+public class BoletimEstudantil {
+
+    //Codigo sem Identação
+
+    //public static void main(String[] args) {
+    //int mediaFinal = 6;
+    //if (mediaFinal<6)
+    //System.out.print("REPROVADO");
+    //else if (mediaFinal==6)
+    //System.out.print("PROVA MINERVA");
+    //else
+    //System.out.print("APROVADO");
+    //}
+
+    //Com Identação
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+        if (mediaFinal < 6)
+            System.out.print("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.print("PROVA MINERVA");
+        else
+            System.out.print("APROVADO");
+    }
+}

6527d85b8fb4b9c0751c32ab3ec9079b3599214a
Gabrieldsmon <gabrieldsmon@gmail.com>
2025-01-05 11:47:58 -0300

    Criando Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,103 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        /*classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?";
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+        */
+
+        /* x repeticao
+        int numero = 5;
+
+        numero = numero +1;
+        numero++;
+
+        System.out.println( ++ numero);
+        System.out.println( -- numero);
+        System.out.println(numero);
+
+        invertendo a variavel que e "true" para "false"
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+
+        variavel = !variavel;
+        System.out.println(variavel);
+        /*
+
+        /* classe Operadores.java
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        //EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if(a==b)
+        resultado = "verdadeiro";
+        else
+        resultado = "falso";
+
+
+        //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        String resultado = (a==b) ? "verdadeiro" : "false";
+
+        System.out.println(resultado);
+        */
+
+        /*String nomeUm = "Gabriel";
+        String nomeDois =  new String("Gabriel");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if (numero1 < numero2) {
+            System.out.println(" a nossa condição é verdadeira ");
+
+        }
+
+        System.out.println(" numeroUm e diferente a numeroDois? " + simNao);
+        */
+
+
+        /*
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+
+        /* Aqui estamos utilizando o operador lógico E para fazer a união de duas expressões.
+        É como se estivesse escrito:
+            "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+        */
+        //if (condicao1 && condicao2) {
+        // System.out.println(" as duas condições são verdadeiras ");
+
+        //}
+       /*
+       //Se condicao1 OU condicao2 for verdadeira, executar código.
+        if(condicao1 || condicao2)
+        System.out.print("Um dos valores precisa ser verdadeiro");
+        */
+
+    }
+
+}
+

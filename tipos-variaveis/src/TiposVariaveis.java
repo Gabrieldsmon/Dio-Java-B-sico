9bee2fd2b14369f56b5865432d1eb95013d499f9
Gabrieldsmon <gabrieldsmon@gmail.com>
2025-01-05 11:54:06 -0300

    Criando projeto de uma Smart TV

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,6 @@
 public class TiposVariaveis {
 
-    /*int idade; //Tipo "int", nome "idade", com nenhum valor atribuído.
+    /* int idade; //Tipo "int", nome "idade", com nenhum valor atribuído.
     int anoFabricacao = 2021; //tipo "int", nome "anoFabricacao", com valor 2021.
     double salarioMinimo = 2.500; //tipo "double", nome "salarioMinimo", valor 2.500.
     */
